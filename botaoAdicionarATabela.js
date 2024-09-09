7d530e90db30f2b4a20c3352a269a9ef97aadcf8
André Izidoro <124374527+andreizidoro123@users.noreply.github.com>
2024-09-09 08:48:58 -0300

    Update botaoAdicionarATabela.js

--- botaoAdicionarATabela.js
+++ botaoAdicionarATabela.js
@@ -1,1 +1,43 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    var formulario = document.querySelector("#form-adiciona");
+    var paciente = obtemfuncionario(formulario);
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+});
 
+function obtemfuncionario(formulario)
+{
+
+    var paciente = {
+        nome: formulario.nome.value,
+        peso: formulario.peso.value,
+        altura: formulario.altura.value,
+        gordura: formulario.gordura.value,
+        imc: calculaImc(formulario.peso.value, formulario.altura.value)
+    }
+    return paciente;
+}
+function montaTd(dado, classe)
+{
+    var td = document.createElement("td");
+    td.classList.add(classe);
+    td.textContent = dado;
+    return td;
+}
+
+function montaTr(paciente)
+{
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+    return pacienteTr;
+
+}
